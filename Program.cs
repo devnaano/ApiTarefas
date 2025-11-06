184caf66442c21d82299b2c26691f11b6fb10661
Carlos Nascimento <naano.dev@gmail.com>
2025-11-06 10:21:43 -0300

    chore: Adicionando Injeção de Dependencia no Program.cs do Service e Repository

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using ApiTarefas.Interfaces;
+using ApiTarefas.Repositories;
 using ApiTarefas.Repositories.Context;
+using ApiTarefas.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+builder.Services.AddScoped<ITarefaService, TarefaService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("sqlserver")));
